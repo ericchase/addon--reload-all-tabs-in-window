1f8d5496477c9f78892e3f2567f38e18a1c808c9
ericchase <github@eric.addymail.com>
2024-06-06 21:53:55 -0500

    add store page menu items
    Signed-off-by: ericchase <github@eric.addymail.com>

--- src/service-worker.js
+++ src/service-worker.js
@@ -20,6 +20,35 @@ chrome.action.onClicked.addListener(async (currentTab) => {
   }
 });
 
+// chrome.contextMenus.create({
+//   contexts: ['action'],
+//   id: 'open-store-page',
+//   title: 'Open Store Page',
+// });
+chrome.contextMenus.create({
+  contexts: ['action'],
+  id: 'open-store-page-chrome',
+  // parentId: 'open-store-page',
+  title: 'Open Chrome Web Store Page',
+});
+chrome.contextMenus.create({
+  contexts: ['action'],
+  id: 'open-store-page-firefox',
+  // parentId: 'open-store-page',
+  title: 'Open Firefox Browser Add-ons Page',
+});
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  switch (info.menuItemId) {
+    case 'open-store-page-chrome':
+      chrome.tabs.create({ url: 'https://chromewebstore.google.com/detail/reload-all-tabs-in-window/fobjljihdlfbamijbmadjkkehmlleaoa' });
+      break;
+    case 'open-store-page-firefox':
+      chrome.tabs.create({ url: 'https://addons.mozilla.org/en-US/firefox/addon/reloadalltabs-inwindow/' });
+      break;
+  }
+});
+
 /** @param {chrome.tabs.Tab} tab */
 function reloadTab(tab) {
   try {
