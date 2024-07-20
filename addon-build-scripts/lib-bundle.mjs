0e7231b1cbf7e27889f093fd6cda4e1f89203cc3
ericchase <github@eric.addymail.com>
2024-07-20 17:43:36 -0500

    had a bug in my build tool
    Signed-off-by: ericchase <github@eric.addymail.com>

--- addon-build-scripts/lib-bundle.mjs
+++ addon-build-scripts/lib-bundle.mjs
@@ -8,5 +8,5 @@ export async function bundle(browser, manifest) {
   await createDirectory(`./release/${browser}`);
   const archive_name = `${toSnakeCase(manifest.get('name'))}-v${manifest.get('version')}.zip`;
   await deleteFile(`./release/${browser}/${archive_name}`);
-  run('7z', ['a', '-tzip', `./release/${browser}/${archive_name}`, `./build/${browser}/*`]);
+  await run('7z', ['a', '-tzip', `./release/${browser}/${archive_name}`, `./build/${browser}/*`]);
 }
